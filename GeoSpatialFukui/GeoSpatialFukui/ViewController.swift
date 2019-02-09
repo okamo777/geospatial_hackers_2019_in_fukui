e861629f55b23c7dce937409c8de26f0e9cb7099
nomunomu <black_sleepman_-.-@au.com>
2019-02-09 13:59:48 +0900

    initial commit. using cocoapods. must install `cocoapods`

---
+++ GeoSpatialFukui/GeoSpatialFukui/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeoSpatialFukui
+//
+//  Created by 野村弘樹 on 2019/02/09.
+//  Copyright © 2019 Hiroki Nomura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
