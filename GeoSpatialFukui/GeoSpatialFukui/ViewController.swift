3e1c453ac30566edbbda0716696ae3b0fa38a03b
okamo777 <okamohyon0327@gmail.com>
2019-02-10 11:23:30 +0900

    バグなう

--- GeoSpatialFukui/GeoSpatialFukui/ViewController.swift
+++ GeoSpatialFukui/GeoSpatialFukui/ViewController.swift
@@ -1,20 +1,8 @@
-//
-//  ViewController.swift
-//  GeoSpatialFukui
-//
-//  Created by 野村弘樹 on 2019/02/09.
-//  Copyright © 2019 Hiroki Nomura. All rights reserved.
-//
-
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-
+class ViewController: UIViewController{
+    
+    @IBOutlet var mapView: MKMapView!
 }
-
